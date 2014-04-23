49e172736dfad3531138b8ea6a9f199bcc9d292b
Ricardo Gonzalez Tellez <ricardo@softco.mx>
2014-04-23 17:07:44 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RicardoAGT::Application.config.secret_key_base = '8040a7a8db6950e19642722a0531c29d6b85484e0ebcda6c7cfc7df7f9d84626959e8c80b0f6efb96061df48e19c3ccea66a514798a3428f697625d376fc0b74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RicardoAGT::Application.config.session_store :cookie_store, key: '_RicardoAGT_session'
